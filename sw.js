76026c7e7b2f11f50879a561cc8c8c65624543af
Paul M <167044181+pmeldrum@users.noreply.github.com>
2025-09-19 11:32:31 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
-self.addEventListener('install', e=>self.skipWaiting()); self.addEventListener('activate', e=>self.clients.claim());
+
+const CACHE = 'llewie-httpd-v1';
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll([
+    '/', '/index.html', '/manifest.json',
+    '/icon-192.png', '/icon-512.png'
+  ])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => self.clients.claim());
+self.addEventListener('fetch', (e) => {
+  const {request} = e;
+  if (request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(request).then(res => res || fetch(request).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE).then(c => c.put(request, copy));
+      return r;
+    }).catch(() => caches.match('/index.html')))
+  );
+});
